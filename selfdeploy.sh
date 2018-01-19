dc6644df3e00f4b30892fcd0c70df93a47414ff5
Umberto Villa <uvilla@ices.utexas.edu>
2018-01-19 00:42:48 +0000

    selfdeploy.sh edited online with Bitbucket

--- selfdeploy.sh
+++ selfdeploy.sh
@@ -1,5 +1,6 @@
 git clone https://bitbucket.org/hippylibdev/hippylib-hub.git
 cd hippylib-hub
-sudo ./build_hippyhub.sh
-sudo ./run_hippyhub.sh
-sudo docker logs hippy-hub >& hippyhub.out
+git clone https://github.com/hippylib/hippylib.git
+sudo docker build -t hippylib/hub .
+sudo docker run -td -p 80:8000 --name=hippyhub hippylib/hub
+sudo docker logs hippyhub >& hippyhub.out
