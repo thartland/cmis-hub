e07071c0f88901be4c4439677253580d8ab47b46
Umberto Villa <umberto.villa@gmail.com>
2018-01-18 20:24:43 -0600

    Update selfdeploy.sh

--- selfdeploy.sh
+++ selfdeploy.sh
@@ -1,6 +1,7 @@
+#!sh
 git clone https://github.com/hippylib/hippylib-hub.git
 cd hippylib-hub
 git clone https://github.com/hippylib/hippylib.git
 sudo docker build -t hippylib/hub .
 sudo docker run -td -p 80:8000 --name=hippyhub hippylib/hub
-sudo docker logs hippyhub >& hippyhub.out
+sudo docker logs hippyhub > hippyhub.out
