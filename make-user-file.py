94af960fdea8b746c76766dd2f1e22d4277ff74a
Tucker Hartland <tucker.hartland@gmail.com>
2022-09-22 23:35:28 -0700

    randomly generated passwords

--- make-user-file.py
+++ make-user-file.py
@@ -1,12 +1,20 @@
 import csv
+import string
+import random
+
 
 header = ['user', 'password']
 
 numUsers = 100
+pwdLength = 6
+
 with open('users.csv', 'w', encoding='UTF8') as f:
     writer = csv.writer(f)
     writer.writerow(header)
-
     for i in range(numUsers):
-        data = ['user'+str(i), 'MDS22']
+        pwd_array = random.choices(string.ascii_letters, k=pwdLength)
+        pwd = ''
+        for j in range(pwdLength):
+            pwd = pwd + pwd_array[j]
+        data = ['user'+str(i), pwd]
         writer.writerow(data)
